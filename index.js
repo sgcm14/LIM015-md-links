c5eee5096b46778047a082b1fdbcfa65875ae949
sgcm14 <sgcm14@gmail.com>
2021-08-25 11:27:36 -0500

    sprint 1
    Creación del diagrama, del package json, correr test de prueba, leer un documento

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 module.exports = () => {
   // ...
 };
+
+const fs = require('fs');
+
+console.log('hola');
+const md= fs.readFileSync('./pruebaDocs/leer.md','utf8');
+console.log(`esto es md:\n`,md);
+
+const txt= fs.readFileSync('./pruebaDocs/contenido.txt','utf8');
+console.log(`esto es txt:\n`,txt);
+
+const readme= fs.readFileSync('readme.md','utf8');
+console.log(`esto es readme:\n`,readme);
---
+++ pruebaDocs/leer.md
@@ -0,0 +1,4 @@
+https://github.com/sgcm14/LIM015-md-links
+https://github.com/sgcm14/LIM015-md-links
+https://github.com/
+** hola mundo **
