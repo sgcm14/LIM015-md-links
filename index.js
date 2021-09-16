0af95bb00e5d6b02a75e5190e044f11f8ae06d72
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-09-15 19:06:37 -0500

    Trying to read a file

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 module.exports = () => {
   // ...
 };
+
+// read file
+// const fs = require('fs');
+
+// fs.readFile('./README.md', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// To access path
+const path = require('path');
+//ruta relative
+const route = 'README.md';
+//ruta absolute
+const routeOne = 'C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\README.md';
+const isAbsolute = path.isAbsolute(route);
+console.log(isAbsolute);
