dd075b56901b7d0cba066d958a5bb5a373b896d6
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-09-22 11:19:11 -0500

    feat/issue #12 function to read and find file/directory

---
+++ src/File/File3/example.md
@@ -0,0 +1,3 @@
+[File system - Documentación oficial (en inglés)](https://nodejs.org/api/fs.html)
+[Path - Documentación oficial (en inglés)](https://nodejs.org/api/path.html)
+[Sitio oficial de GitHub Pages](https://pages.github.com/)
--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 
 //To access the file if it exists BOOLEAN
 function pathExists(route) {
@@ -8,10 +8,10 @@ function pathExists(route) {
 }
 
 //To convert the path to absolute
-function resolvePathA(route) {
-  return path.resolve(route);
-}
-
+// function resolvePathA(route) {
+//   return path.resolve(route);
+// }
+const resolvePathA = (route) => path.isAbsolute(route) ? route : path.resolve(route)
 //To know is an absolute path
 //ruta relative
 const route = "README.md";
@@ -19,18 +19,39 @@ const route = "README.md";
 const routeOne =
   "C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\README.md";
 const isAbsolute = path.isAbsolute(route);
-console.log(isAbsolute);
+//console.log(isAbsolute);
 
 //read file
-fs.readFile("./README.md", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
+// fs.readFile("./README.md", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 //To detec if path is a file or not BOOLEAN
 function findDirectory(route) {
     return fs.statSync(route).isDirectory();
 };
+
+//To read and find file and directory
+const readFileandDirectory =(route) => {
+  let newArray = []
+  if(findDirectory(route)) {
+    const arrayDirectory = fs.readdirSync(route)
+    arrayDirectory.forEach((file) => {
+      const joinArray = path.join(route, '/', file)
+      if(findDirectory(joinArray)) {
+        newArray = newArray.concat(readFileandDirectory(joinArray))
+      }else if (path.extname(joinArray) === '.md'){
+        newArray.push(joinArray)
+      }
+    })
+  }
+  return newArray
+}
+console.log(readFileandDirectory('C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\src'))
+//console.log(findDirectory('C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\src'))
+//console.log(findDirectory('C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\src\\index.js'))
+//console,log('C:\Users\Usuario\Documents\LABORATORIA\LIM015-md-links\README.md')
