32e7b479aa6d2a359edbee923b34766f13c41d16
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-09-27 11:38:53 -0500

    fix function extractTheLinks

--- src/api.js
+++ src/api.js
@@ -1,38 +1,34 @@
 const path = require('path');
 const fs = require('fs');
-const marked = require('marked');
-const fetch = require('node-fetch');
 const userPath = process.argv[2];
 
-//examples of relative path and absolute path
-const relativePath = "README.md";
-const absolutePath ="C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\README.md";
 
 //To convert the path to absolute
-const isAbsolute = path.isAbsolute(absolutePath)
+const tobeAbsolute = (userPath) =>path.isAbsolute(userPath)
+//console.log(tobeAbsolute(userPath))
 //To validate is path is absolute and return if it is relative
-const resolvePathA = (relativePath) => path.isAbsolute(relativePath) ? relativePath : path.resolve(relativePath)
+const resolvePathA = (route) => path.isAbsolute(route) ? route : path.resolve(route)
 
 //To access the file if it exists BOOLEAN
-function pathExists(absolutePath) {
-  return fs.existsSync(absolutePath);
+function pathExists(userPath) {
+  return fs.existsSync(userPath);
 }
 
 //To detec if path is a file or not BOOLEAN
-function findDirectory(absolutePath) {
-  return fs.statSync(absolutePath).isDirectory();
+function findDirectory(userPath) {
+  return fs.statSync(userPath).isDirectory();
 };
 
 //To read and find file and directory
-const readFileandDirectory = (absolutePath) => {
+const readFileAndDirectory = (userPath) => {
   let newArray = []
-  if(findDirectory(absolutePath)) {
-    const arrayDirectory = fs.readdirSync(absolutePath)
+  if(findDirectory(userPath)) {
+    const arrayDirectory = fs.readdirSync(userPath)
     arrayDirectory.forEach((file) => {
-      const joinArray = path.join(route, '/', file)
-      console.log(joinArray)
+      const joinArray = path.join(userPath, '/', file)
+      //console.log(joinArray)
       if(findDirectory(joinArray)) {
-        newArray = newArray.concat(readFileandDirectory(joinArray))
+        newArray = newArray.concat(readFileAndDirectory(joinArray))
       }else if (path.extname(joinArray) === '.md'){
         newArray.push(joinArray)
       }
@@ -40,24 +36,25 @@ const readFileandDirectory = (absolutePath) => {
   }
   return newArray
 }
+//console.log(readFileAndDirectory(userPath))
 
 //To extract the links
-const extractTheLinks = (absolutePath) => {
+const extractTheLinks = (userPath) => {
   const arryLinks = []
   const regExFile = /\[(.*)\]( *)\(((((https?:\/\/)|(http?:\/\/)|(www\.))[^\s\n)]+)(?=\)))\)/gi
   const regExCorchetes = /\[(.*?)\]/gi
   const regExUrl = /(((https?:\/\/)|(http?:\/\/)|(www\.))[^\s\n)]+)(?=\))/gi
-  readFileAndDirectory(absolutePath).forEach((file) => {
+  readFileAndDirectory(userPath).forEach((file) => {
     const readFile = fs.readFileSync(file).toString()
     const matchFile = readFile.match(regExFile)
     if (matchFile != null) {
       for (let i = 0; i < matchFile.length; i++) {
-        const tag = matchFile[i].match(regExCorchetes)[0].replace('[', '').replace(']', '')
-        const url = matchFile[i].match(regExUrl)[0]
+        const text = matchFile[i].match(regExCorchetes)[0].replace('[', '').replace(']', '')
+        const href = matchFile[i].match(regExUrl)[0]
         const linkObj = {
-          href: url,
-          text: tag,
-          file: route
+          href,
+          text,
+          file
         }
         arryLinks.push(linkObj)
       }
@@ -65,4 +62,6 @@ const extractTheLinks = (absolutePath) => {
   })
   return arryLinks
 }
+//console.log(extractTheLinks(userPath))
 console.log(extractTheLinks(userPath))
+//C:\\Users\\Usuario\\Documents\\LABORATORIA\\LIM015-md-links\\src
