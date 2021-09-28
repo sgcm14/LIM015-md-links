155c00e7fedaacf8421d3ef59e9208e468df3add
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-09-28 11:52:08 -0500

    feat/issue#17 function MD-LINKS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 const {
-    tobeAbsolute,
-    resolvePathA,
-    pathExists,
-    findDirectory,
-    readFileAndDirectory,
-    extractTheLinks,
-    confirmOptions,
-  } = require('./api');
+  tobeAbsolute,
+  resolvePathA,
+  pathExists,
+  readFileAndDirectory,
+  extractTheLinks
+} = require('./api')
 
-const mdLinks = (path, options = {}) => {
-    return new Promise((resolve, reject) => {
-        
-    })
+const mdLinks = (path, options = { validate: false }) => {
+  return new Promise((resolve, reject) => {
+    if (pathExists(path)) {
+      if (tobeAbsolute(path)) {
+        const fileMD = readFileAndDirectory(path)
+        const fileLinks = extractTheLinks(fileMD)
+        resolve(fileLinks)
+        // confirmOptions(fileLinks).then(resolve)
+      } else {
+        const absoluteRoute = resolvePathA(path)
+        const arrayMD = readFileAndDirectory(absoluteRoute)
+        const arrayObj = extractTheLinks(arrayMD)
+        // confirmOptions(arrayObj).then(resolve)
+        resolve(arrayObj)
+      }
+    } else {
+      reject(console.log('No existe la ruta'))
+    }
+  })
 }
+
+module.exports = { mdLinks }
